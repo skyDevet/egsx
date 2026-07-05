fcdddf5f18fafced754c7f2b499153e8e12c69c1
skyDevet <skyn4302@gmail.com>
2026-07-06 00:57:20 +0300

    Deploy: 2026-07-06 00:57:20

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
-export default defineConfig({
-  plugins: [preact()],
-  base: '/egsx/'
+export default defineConfig(({ mode }) => {
+  // Force GitHub Pages base
+  const baseUrl = '/egsx/'
+  
+  return {
+    base: baseUrl,
+    plugins: [preact()],
+    server: { host: true, port: 3000 },
+    build: { outDir: 'dist', sourcemap: false }
+  }
 })
